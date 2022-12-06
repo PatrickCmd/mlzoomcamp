252eb1e52df913c394790587c0b6334c89230314
patrickcmd <pwalukagga@gmail.com>
2022-12-06 22:07:45 +0300

    Week 10 submission

--- course-zoomcamp/cohorts/2022/05-deployment/homework/q6_test.py
+++ course-zoomcamp/cohorts/2022/05-deployment/homework/q6_test.py
@@ -1,9 +1,15 @@
 import requests
+from time import sleep
 
 
 url = "http://localhost:9696/predict"
 
 client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
-response = requests.post(url, json=client).json()
+# response = requests.post(url, json=client).json()
 
-print(response)
+# print(response)
+
+while True:
+    sleep(0.1)
+    response = requests.post(url, json=client).json()
+    print(response)
